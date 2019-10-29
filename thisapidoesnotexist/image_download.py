881b7a0bcdf4f37efe5f184ef54358182141738b
Miguel Lopes <miguel.lopes.filho@usp.br>
2019-10-29 16:42:31 -0300

    Deixando de usar métodos protegidos para carregar a imagem

--- thisapidoesnotexist/image_download.py
+++ thisapidoesnotexist/image_download.py
@@ -5,15 +5,13 @@ __all__ = ['get_person', 'get_cat']
 
 def get_person():
     person = Person()
-    person._get_image()
-    person._get_hash()
+    person.new_image()
 
     return person
 
 
 def get_cat():
     cat = Cat()
-    cat._get_image()
-    cat._get_hash()
+    cat.new_image()
 
     return cat
