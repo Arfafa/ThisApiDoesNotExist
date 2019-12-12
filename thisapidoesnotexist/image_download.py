635ca3f6a513c6a641bb39e2bc7f2b0c9cb11ac8
Miguel Lopes <miguel.lopes.filho@usp.br>
2019-12-12 19:55:42 -0300

    add docstring

--- thisapidoesnotexist/image_download.py
+++ thisapidoesnotexist/image_download.py
@@ -4,6 +4,8 @@ __all__ = ['get_person', 'get_cat']
 
 
 def get_person():
+    """Função que retorna um objeto da classe Person
+    com uma imagem já carregada"""
     person = Person()
     person.new_image()
 
@@ -11,6 +13,8 @@ def get_person():
 
 
 def get_cat():
+    """Função que retorna um objeto da classe Cat
+    com uma imagem já carregada"""
     cat = Cat()
     cat.new_image()
 
